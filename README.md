20a6a9830491b69d4e30979a38daff4c3350e330
Magnus Manske <magnusmanske@gmail.com>
2023-04-05 12:04:17 +0000

    misc

---
+++ README.md
@@ -0,0 +1,1 @@
+https://pauken.toolforge.org/
--- vue.js
+++ vue.js
@@ -11,12 +11,16 @@ $(document).ready ( function () {
     vue_components.toolname = 'pauken' ;
     Promise.all ( [
         vue_components.loadComponents ( ['wd-date','wd-link','tool-translate','tool-navbar','commons-thumbnail','widar','autodesc','typeahead-search','value-validator',
+            'vue_components/entry.html',
             'vue_components/main-page.html',
+            'vue_components/learn-page.html',
             ] )
     ] )
     .then ( () => {
         const routes = [
             { path: '/', component: MainPage , props:true },
+            { path: '/learn/:items_param', component: LearnPage , props:true },
+            { path: '/learn/:items_param/:learn_language', component: LearnPage , props:true },
             ] ;
         router = new VueRouter({routes}) ;
         app = new Vue ( { router } ) .$mount('#app') ;
